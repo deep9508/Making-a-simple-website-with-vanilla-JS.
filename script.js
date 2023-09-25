32a78f92dfa89ccb50ea74b777f40c0f82ad37c2
DEEPAK KUMAR <112052287+deep9508@users.noreply.github.com>
2023-09-25 09:03:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,125 @@
+(function () {
+  "use strict";
+  // helper functions
+  function preventThrottel() {
+    let isThrottled = false;
+  
+    return function (callback, delay) {
+        if (!isThrottled) {
+            isThrottled = true;
+            callback();
+            setTimeout(() => {
+                isThrottled = false;
+            }, delay);
+        }
+    }
+  }
+
+  // variable 
+  document.getElementById("buttonEvent1").addEventListener('click', handleButtonClick);
+  document.getElementById("buttonEvent2").addEventListener('click', handleButtonClick);
+  document.getElementById("buttonEvent3").addEventListener('click', handleButtonClick);
+  document.getElementById("userRange").addEventListener('change', onSliderInputChange);
+  window.addEventListener('scroll', runOnScroll)
+
+  const card1 = document.getElementById("card1"),
+    card2 = document.getElementById("card2"),
+    card3 = document.getElementById("card3");
+  
+  // Initialization of Throttle Function
+  const throttledSafeLoadMoreImage= preventThrottel();
+
+  //execute on load
+  document.addEventListener('DOMContentLoaded', function () {
+    console.log('hello, my name is sarvesh, I have made this project to showcase my front-end skills, also this app is enabled with ci/cd using gitlab ci/cd pipelines');
+    console.log('youTube link: https://youtu.be/ZObbAqwrQx4');
+    onSliderInputChange(); 
+    throttledSafeLoadMoreImage(loadMoreImage, 2000);
+  }, false);
+
+  // event basedfunctions
+  function handleButtonClick(event) {
+    document.getElementById('selectedPriceValue').setAttribute('value', event.target.value)
+  }
+
+  function onSliderInputChange(event) {
+    const sliderValue = event?.target?.value || 0;
+    document.getElementById('selectedUser').innerText = sliderValue;
+
+    function clearClass() {
+      card1.classList.remove("border", "border-primary");
+      card1.firstElementChild.classList.remove("bg-primary")
+      card2.classList.remove("border", "border-primary");
+      card2.firstElementChild.classList.remove("bg-primary")
+      card3.classList.remove("border", "border-primary");
+      card3.firstElementChild.classList.remove("bg-primary")
+    }
+
+    if (sliderValue >= 0 && sliderValue <= 10) {
+
+      clearClass();
+      card1.classList.add("border", "border-primary");
+      card1.firstElementChild.classList.add("bg-primary")
+
+    } else if (sliderValue > 10 && sliderValue <= 20) {
+
+      clearClass();
+      card2.classList.add("border", "border-primary");
+      card2.firstElementChild.classList.add("bg-primary")
+
+    } else {
+
+      clearClass();
+      card3.classList.add("border", "border-primary");
+      card3.firstElementChild.classList.add("bg-primary")
+
+    }
+  }
+
+
+  function runOnScroll() {
+    if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight - 1){
+      throttledSafeLoadMoreImage(loadMoreImage, 2000);
+    }
+  }
+  
+
+  // normal functions
+  //loadMoreImage getting called multiple times if user is not waiting, and start scrolling up-down again
+  function loadMoreImage() {
+    console.log('loadMoreImageCalled');
+    let defaultLoad = 8;
+    let container= document.getElementById('imgGalery');
+    const loader= document.getElementById('loader');
+
+    loader.classList.remove('d-none');
+    loader.classList.add('d-flex');
+    const promises= []
+
+    let i = 0;
+    while (i < defaultLoad) {
+      const currPromise= fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(data => {
+          const img = document.createElement('img');
+          img.src = data.message
+          img.width = 300
+          img.alt = "img not found"
+          img.classList.add('col-12', 'col-md-6', 'col-lg-4', 'col-xl-3', 'my-3');
+          container.appendChild(img);
+        })
+      i++;
+
+      promises.push(currPromise)
+    }
+
+    // hide the loader after getting all result
+    Promise.all(promises).then((res)=>{
+      loader.classList.add('d-none');
+      loader.classList.remove('d-flex');
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }
+
+})()
